f9563885d7421dc46f4f693bc8efc376264611c0
anokata <quartenium@gmail.com>
2017-04-07 17:50:20 +0300

    chg

--- Linux/bin/hub.sh
+++ Linux/bin/hub.sh
@@ -1,3 +1,9 @@
-cd ~/ydsk/txthub/
-git add . && git commit -m "txt" && git push origin master 
+cd ~/ydsk/doc/
+git add . && git commit -m "doc" 
+git push origin master 
+git push origin_bitbucket master
+
+cd ~/ydsk/dotfiles/
+git add . && git commit -m "chg" 
+git push origin master 
 git push origin_bitbucket master
