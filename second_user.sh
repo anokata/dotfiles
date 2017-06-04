dc9bf4e4e7dfe97ef586405e5d44d101a1ac4a67
anokata <anokata8@mail.ru>
2017-06-04 19:43:32 +0300

    scripts on arch setup

--- second_user.sh
+++ second_user.sh
@@ -5,3 +5,7 @@ cp dotfiles/.vimrc ~/
 sudo pacman -S ctags
 git config --global user.name "anokata"
 git config --global user.email "anokata8@mail.ru"
+cat dotfiles/bashadd >> ~/.bashrc 
+cp dotfiles/.tmux.conf ~/
+sudo pacman -S dmenu
+cp dotfiles/Linux/bin/ -r ~/
