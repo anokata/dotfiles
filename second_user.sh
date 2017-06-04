642e183b31cd8dc3d4bc140fc508d2bf161b9a4d
anokata <anokata8@mail.ru>
2017-06-04 20:00:30 +0300

    add setup script

--- second_user.sh
+++ second_user.sh
@@ -1,11 +1,22 @@
+sudo pacman -S ctags
+sudo pacman -S dmenu
+sudo pacman -S git
+sudo pacman -S xbindkeys
+sudo pacman -S xorg-xmodmap
+sudo pacman -S ttf-dejavu
+
+xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
+git config --global user.name "anokata"
+git config --global user.email "anokata8@mail.ru"
+
 git clone http://github.com/anokata/dotfiles.git
+mkdir -p ~/.config/i3
 mkdir -p ~/.vim/autoload
 cp dotfiles/Linux/configs/.vim/autoload/plug.vim ~/.vim/autoload/
 cp dotfiles/.vimrc ~/
-sudo pacman -S ctags
-git config --global user.name "anokata"
-git config --global user.email "anokata8@mail.ru"
 cat dotfiles/bashadd >> ~/.bashrc 
 cp dotfiles/.tmux.conf ~/
-sudo pacman -S dmenu
 cp dotfiles/Linux/bin/ -r ~/
+cp dotfiles/.xprofile ~/
+cp dotfiles/.xbindkeysrc ~/
+cp dotfiles/Linux/configs/i3/config ~/.config/i3/
---
+++ system_setup/firstarch.sh
@@ -0,0 +1,11 @@
+pacman -Syu
+pacman -S pptpclient
+pacman -S grub
+pacman -S sudo tmux vim
+pacman -S i3 xorg-server xorg
+pacman -S xorg-xdm
+pacman -S xf86-video-intel
+pacman -S xterm
+systemctl enable xdm
+
+#makeinet
