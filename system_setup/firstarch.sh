309ecbb5b7629c19bd2a65e03da60e7faf6d40e8
anokata <quartenium@gmail.com>
2017-06-17 16:04:01 +0300

    arch config

--- system_setup/firstarch.sh
+++ system_setup/firstarch.sh
@@ -3,21 +3,9 @@ pacman -S pptpclient grub efibootmgr os-prober
 
 #makeinet
 
-sudo pacman -S sudo tmux vim
-sudo pacman -S ctags git tree htop
+pacman -S sudo tmux vim
+pacman -S ctags git tree htop
 # X specific
-sudo pacman -S i3 xorg-server xorg
-sudo pacman -S xorg-xdm
-sudo pacman -S xf86-video-intel
-sudo pacman -S xterm
-sudo pacman -S dmenu
-sudo pacman -S xbindkeys
-sudo pacman -S xorg-xmodmap
-sudo pacman -S ttf-dejavu
-sudo pacman -S i3 xorg-server xorg
-sudo pacman -S xorg-xdm
-sudo pacman -S xf86-video-intel
-sudo pacman -S xterm
-sudo pacman -S xcompmgr
-sudo pacman -S transset-df
+pacman -S xorg-server xorg xorg-xdm xf86-video-intel xterm dmenu xbindkeys xorg-xmodmap ttf-dejavu i3 xcompmgr transset-df xorg-xinit xorg-xconsole
 systemctl enable xdm
+pacman -S coreutils
--- system_setup/second_user.sh
+++ system_setup/second_user.sh
@@ -2,8 +2,5 @@
 xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
 git config --global user.name "anokata"
 git config --global user.email "anokata8@mail.ru"
-
-git clone http://github.com/anokata/dotfiles.git
-#./dotfiles/vans/ansible.sh
-cp dotfiles/Linux/bin/ -r ~/
+cp dotfiles/bin/ -r ~/
 
