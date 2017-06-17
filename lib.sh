e5e2095338113e70023fb767d3718863c68ce015
anokata <quartenium@gmail.com>
2017-06-17 10:47:43 +0300

    chg (autocommit)

--- lib.sh
+++ lib.sh
@@ -6,6 +6,7 @@ function say() {
     echo "$(tput setaf 0; tput setab 7;)$1$(tput sgr 0;)"
 }
 function git_dirty {
-    [[ $(git status --porcelain) != "" ]] && echo -e " $RED*$WHITE"
+    status=$(git status --porcelain) 
+    if [[ $status != "" ]]; then echo -e " $RED*$WHITE"; fi
 }
 
