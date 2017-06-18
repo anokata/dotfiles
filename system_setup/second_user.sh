febcf5f7b7c3e9d6fa6bbb307f26eb05619ca6a5
anokata <anokata8@mail.ru>
2017-06-18 10:01:09 +0300

    chg (autocommit)

--- system_setup/second_user.sh
+++ system_setup/second_user.sh
@@ -4,3 +4,5 @@ git config --global user.name "anokata"
 git config --global user.email "anokata8@mail.ru"
 cp dotfiles/bin/ -r ~/
 
+sudo localectl --no-convert set-x11-keymap ru,us pc104 ,qwerty grp:alt_shift_toggle
+setxkbmap -model pc104 -layout us,ru -variant ,winkeys -option grp:alt_shift_toggle
