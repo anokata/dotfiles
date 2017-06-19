5cc066b23a9db8a503f439cbcd19ea904845e518
anokata <anokata8@mail.ru>
2017-06-19 18:02:29 +0300

    (autocommit)

---
+++ bin/lib/net.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for d in $(/bin/ls -1 /sys/class/net); do 
+    first=$(echo $d | head -c1)
+    if [[ $first = 'e' ]]; then
+        ETH=$d
+    elif [[ $first = 'w' ]]; then
+        WIFI=$d
+    fi
+done
--- lib.sh
+++ lib.sh
@@ -4,6 +4,7 @@ source ~/dotfiles/bin/lib/distro.sh
 source ~/dotfiles/bin/lib/tmux.sh
 source ~/dotfiles/bin/lib/alias.sh
 source ~/dotfiles/bin/lib/ssh.sh
+source ~/dotfiles/bin/lib/net.sh
 #source ~/dotfiles/bin/lib/*.sh #???
 
 function say() {
@@ -26,3 +27,45 @@ function git_dirty {
 getlast() { # Need in shell, not rename with _
     fc -ln "$1" "$1" | sed '1s/^[[:space:]]*//'
 }
+
+### Develop zone ###
+
+function _arch_net_connect() {
+    #TODO if not ping ya.ru? or on first terminal
+    # if uptime < 5m?
+    # https://wiki.archlinux.org/index.php/Getty#Virtual_console
+    echo -n "Up $ETH..."
+    sudo ip link set $ETH up
+    sleep 0.1
+    echo 'ok'
+    dhcpcd
+    sleep 0.1
+    if ping -c1 inet.atel.me >/dev/null 2>/dev/null; then
+        # JUST Call net.arch_pptp
+        echo 'welcome home (ppp)'
+        #pon atel
+        for i in 1 2 3 4 5; do
+        if ip link show ppp0 >/dev/null 2>/dev/null ; then
+            break
+        fi
+        echo "$i) Wait sec for ppp0..."
+        sleep 1
+        done
+        sleep 1
+        dhcpcd 
+        sleep 1
+        ip route add default dev ppp0
+    else
+        echo 'try wifi'
+        #TODO
+    fi
+}
+
+function _dev() {
+    case $DIST_SYM in
+        A) _arch_net_connect;;
+        D) echo debian;;
+        U) echo 'ubuntu';;
+    esac
+}
+_dev
