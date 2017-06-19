1000f483252fd85190cefbc75590913ab66ef21c
anokata <anokata8@mail.ru>
2017-06-19 10:41:13 +0300

    doc (autocommit)

--- lib.sh
+++ lib.sh
@@ -30,16 +30,46 @@ function git_dirty {
 }
 
 #OS=$(lsb_release -si)
+#http://linuxmafia.com/faq/Admin/release-files.html
+DIST_COLOR=$W
+DIST_SHORT=-
 get_distro_name () {
     if [ -f /etc/lsb-release ]; then
         . /etc/lsb-release
         DISTRO=$DISTRIB_ID
+        DIST_SHORT='U'
+        DIST_COLOR=$BC
     elif [ -f /etc/debian_version ]; then
         DISTRO=debian
+        DIST_SHORT='D'
+        DIST_COLOR=$BM
     elif [ -f /etc/redhat-release ]; then
         DISTRO="Red Hat"
         DISTRO=$(cat /etc/redhat-release | cut -d" " -f 1)
-    fi
+        DIST_SHORT='C'
+        DIST_COLOR=$BR
     # for arch
+    elif [ -f /etc/arch-release ]; then 
+        DISTRO="ARCH"
+        DIST_SHORT='A'
+        DISTRO=$(cat /etc/arch-release)
+        DIST_COLOR=$BG
+    fi
+}
+get_distro_name
+DIST_SHORT=[${DIST_COLOR}$DIST_SHORT$W]
+
+TMUX_SESSION_NAME=base
+function _tmux_run () {
+    if [ -z $TMUX ]; then
+        echo 'not in tmux'
+        if tmux has-session -t $TMUX_SESSION_NAME 2>/dev/null; then
+            echo 'attach'
+            tmux attach -t $TMUX_SESSION_NAME
+        else
+            echo 'create'
+            tmux new -s $TMUX_SESSION_NAME
+        fi
+    fi
 }
 
