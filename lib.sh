c25ebc442ca738e8f40cad68cf2cabd586a9c0d7
anokata <anokata8@mail.ru>
2017-06-19 09:05:36 +0300

    chg (autocommit)

--- lib.sh
+++ lib.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 RED='\033[01;31m'
 WHITE='\033[01;00m'
-local RESET="\[\033[0;37m\]" 
-local K="\[\033[0;30m\]"    # black
-local R="\[\033[0;31m\]"    # red
-local G="\[\033[0;32m\]"    # green
-local Y="\[\033[0;33m\]"    # yellow
-local B="\[\033[0;34m\]"    # blue
-local M="\[\033[0;35m\]"    # magenta
-local C="\[\033[0;36m\]"    # cyan
-local W="\[\033[0;37m\]"    # white
+readonly RESET="\[\033[0;37m\]" 
+readonly K="\[\033[0;30m\]"    # black
+readonly R="\[\033[0;31m\]"    # red
+readonly G="\[\033[0;32m\]"    # green
+readonly Y="\[\033[0;33m\]"    # yellow
+readonly B="\[\033[0;34m\]"    # blue
+readonly M="\[\033[0;35m\]"    # magenta
+readonly C="\[\033[0;36m\]"    # cyan
+readonly W="\[\033[0;37m\]"    # white
 # emphasized (bolded) colors
-local BK="\[\033[1;30m\]"
-local BR="\[\033[1;31m\]"
-local BG="\[\033[1;32m\]"
-local BY="\[\033[1;33m\]"
-local BB="\[\033[1;34m\]"
-local BM="\[\033[1;35m\]"
-local BC="\[\033[1;36m\]"
-local BW="\[\033[1;37m\]"
+readonly BK="\[\033[1;30m\]"
+readonly BR="\[\033[1;31m\]"
+readonly BG="\[\033[1;32m\]"
+readonly BY="\[\033[1;33m\]"
+readonly BB="\[\033[1;34m\]"
+readonly BM="\[\033[1;35m\]"
+readonly BC="\[\033[1;36m\]"
+readonly BW="\[\033[1;37m\]"
 
 function say() {
     echo "$(tput setaf 0; tput setab 7;)$1$(tput sgr 0;)"
 }
 
 function git_dirty {
-    status=$(git status --porcelain) 
-    if [[ $status != "" ]]; then echo -e " $RED*$WHITE"; fi
+    local status=$(git status --porcelain 2>/dev/null) 
+    if [[ $status != "" ]]; then echo -e "*"; fi
+}
+
+#OS=$(lsb_release -si)
+get_distro_name () {
+    if [ -f /etc/lsb-release ]; then
+        . /etc/lsb-release
+        DISTRO=$DISTRIB_ID
+    elif [ -f /etc/debian_version ]; then
+        DISTRO=debian
+    elif [ -f /etc/redhat-release ]; then
+        DISTRO="Red Hat"
+        DISTRO=$(cat /etc/redhat-release | cut -d" " -f 1)
+    fi
+    # for arch
 }
 
