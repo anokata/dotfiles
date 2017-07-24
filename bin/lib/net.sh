b12dc84168dc0e88ff69bc3b7be987787ec2471c
anokata <anokata8@mail.ru>
2017-07-24 17:58:57 +0300

    lan ip, grep c code

--- bin/lib/net.sh
+++ bin/lib/net.sh
@@ -5,6 +5,7 @@ for d in $(/bin/ls -1 /sys/class/net); do
     first=$(echo $d | head -c1)
     if [[ $first = 'e' ]]; then
         ETH=$d
+        LAN=$d
     elif [[ $first = 'w' ]]; then
         WIFI=$d
     fi
