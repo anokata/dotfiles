5a70dc7f9e7f5edd785c7848a575338ed106a8e8
quartenium <quartenium@gmail.com>
2017-09-25 18:07:51 +0300

    Colors and work sh chg

--- bin/lib/work.sh
+++ bin/lib/work.sh
@@ -4,3 +4,4 @@ WORK_DIR=~/workprojects
 support=ssh://git@bitbucket.mosreg.ru:7999/sup/self-support.git
 qa=ssh://git@bitbucket.mosreg.ru:7999/sup/qasite.git
 feedback=ssh://git@bitbucket.mosreg.ru:7999/sup/mosreg-survey.git
+feedbackhttp=http://TikhomirovSVl@bitbucket.mosreg.ru/scm/sup/mosreg-survey.git
