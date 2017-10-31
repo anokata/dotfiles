4a1d05b5b780da7e0289fa46bbc1a4c85daf3a30
anokata <anokata8@mail.ru>
2017-10-31 14:18:55 +0300

    add http work prjs git env link

--- bin/lib/work.sh
+++ bin/lib/work.sh
@@ -5,3 +5,5 @@ support=ssh://git@bitbucket.mosreg.ru:7999/sup/self-support.git
 qa=ssh://git@bitbucket.mosreg.ru:7999/sup/qasite.git
 feedback=ssh://git@bitbucket.mosreg.ru:7999/sup/mosreg-survey.git
 feedbackhttp=http://TikhomirovSVl@bitbucket.mosreg.ru/scm/sup/mosreg-survey.git
+qahttp=http://bitbucket.mosreg.ru/scm/sup/qasite.git
+supporthttp=http://bitbucket.mosreg.ru/scm/sup/self-support.git
