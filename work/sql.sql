29d76e8d99ca94220a587c7ee0433c438653e14a
anokata <anokata8@mail.ru>
2017-12-20 17:15:30 +0300

    sql

--- work/sql.sql
+++ work/sql.sql
@@ -1,3 +1,13 @@
+with 
+    q1 as (select 1 as a),
+    q2 as (select 2 as a),
+    q3 as (select 3 as a),
+
+    _1 as (select * from q1, q2, q3),
+    r1 as (select a from q1 union select a from q2)
+
+select * from r1;
+
 select m,k from 
     (select n, m from (
         select 1 as n ,'a' as m
