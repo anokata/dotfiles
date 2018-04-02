cb4feb8336421286a54bb67c5aab0a831ce681e4
anokata <anokata8@mail.ru>
2018-04-02 17:57:40 +0300

    fix

--- bashconf.sh
+++ bashconf.sh
@@ -21,8 +21,8 @@ export ANSIBLE_NOCOWS=1
 #export WORK_DIR=~/work/portal
 export CLICOLOR="YES"
 #export TERM=xterm-256color
-#export TERM=screen-256color
-export TERM=xterm
+export TERM=screen-256color
+#export TERM=xterm
 if [ ! -z $(which java 2&>/dev/null) ]; then
     export JAVA_HOME=$(dirname $(dirname $(dirname $(readlink -f $(which java)))))
 fi
@@ -31,7 +31,7 @@ if [ -x ~/dotfiles/ ]; then
 fi
 
 #export PS1="${DIST_SHORT}$XX\u$W@$OX\A$W\w$R\$(git_dirty)$W\$ "
-export PS1="$GREY\u$GRAY@$GREY\A$GRAY\w$R\$(git_dirty)$GRAY\$ $RESET$B"
+export PS1="$GREY\u$GRAY@$GREY\A$GRAY\w$R\$(git_dirty)$GRAY\$ $RESET"
 
 
 #if [ -f ~/dotfiles/xterm_trans ]; then
