74207be91bedb0c95b838d1ea6bd17861dabb9f8
anokata <anokata8@mail.ru>
2018-06-26 15:31:30 +0300

    work

--- work/zabbixsoap.sh
+++ work/zabbixsoap.sh
@@ -3,6 +3,12 @@
 WSDL="http://10.4.126.20:8091/service_medic_mfc/mfc/mfc_service.wsdl"
 ACTION="GetDictionary"
 DATAFILE="/home/ksi/work/soap/getDictionary_catalog_request.xml"
+OKRESPONSE="CatalogResponse"
+
+WSDL="$1"
+ACTION="$2"
+DATAFILE="$3"
+#OKRESPONSE="$4"
 
 curl \
     --header "Content-Type: text/xml;charset=UTF-8" \
@@ -10,7 +16,8 @@ curl \
     --data @$DATAFILE \
      $WSDL > /tmp/soap 2>&1
 
-cat /tmp/soap | grep CatalogResponse > /dev/null
-#echo $? > /tmp/soap_result
-echo 0
+cat /tmp/soap | grep $OKRESPONSE > /dev/null
+echo $?
+#echo 0
+#{Zabbix server:zabbixsoap.sh["http://10.4.126.20:8091/service_medic_mfc/mfc/mfc_service.wsdl","GetDictionary","/home/ksi/work/soap/getDictionary_catalog_request.xml"].str("0", #1)}=0
 
