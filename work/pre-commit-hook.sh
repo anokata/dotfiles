53514cb06bb157bff2e6144995db6c5cb6d98c2a
anokata <anokata8@mail.ru>
2018-07-10 09:56:54 +0300

    work

--- work/pre-commit-hook.sh
+++ work/pre-commit-hook.sh
@@ -17,12 +17,20 @@ do
         echo "INFO: skipping ${filename} because \"${extension}\""
         continue
     fi
+    IS_API=$(basename $file | grep '_api.py$' > /dev/null; echo $?)
+    IS_SMEV=$(echo $file | grep 'smev' > /dev/null; echo $?)
+    if grep generateDS $file >/dev/null || ( (exit $IS_API) && (exit $IS_SMEV) ); then
+        echo "INFO: skipping ${filename} because generated"
+        continue
+    fi
+
     if ! git diff -U0 $file | pycodestyle $OPTIONS; then
         echo $(git diff -U0 ${file} | pycodestyle $OPTIONS)
         ERRORS=1
     fi
     
     if ! git diff -U0 $file | ./checkpydoc.py ; then
+        echo $(git diff -U0 $file | ./checkpydoc.py )
         ERRORS=1
     fi
     if [ $ERRORS == 0 ]; then 
