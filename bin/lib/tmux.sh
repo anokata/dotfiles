fa082444bb6b98c4e894f044d658d98ba8952760
anokata <anokata8@mail.ru>
2019-08-28 10:10:13 +0300

    tmux

--- bin/lib/tmux.sh
+++ bin/lib/tmux.sh
@@ -18,7 +18,7 @@ function _tmux_run () {
                 tmux split-window -v -c ~/doc/
                 tmux new-window -c ~/dotfiles/net
                 tmux split-window -v -c /mnt/store
-                tmux new-window -c ~/dev/allInOne/corona/takeone
+                tmux new-window -c /mnt/values/flac/Starbox/ # ~/dev/allInOne/corona/takeone
                 tmux split-window -v -c /mnt/store/
                 tmux select-window -t 2
             fi
