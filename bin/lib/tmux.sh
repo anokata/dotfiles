d4a1a0c501f9f2aeee5e6c62301ad39c197aca9f
anokata <anokata8@mail.ru>
2020-05-04 17:29:35 +0300

    chg-cfg

--- bin/lib/tmux.sh
+++ bin/lib/tmux.sh
@@ -18,7 +18,7 @@ function _tmux_run () {
                 tmux split-window -v -c ~/doc/
                 tmux new-window -c ~/dotfiles/ #net
                 tmux split-window -v "vifm"
-                tmux new-window -c ~/dev/
+                tmux new-window -c /mnt/store/hobby/draw
                 tmux split-window -v "vifm"
                 tmux select-window -t 2
                 #tmux new-window "/home/ksi/dotfiles/bin/schedule_control.py"
