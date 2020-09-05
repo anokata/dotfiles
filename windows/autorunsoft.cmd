42a570667dafa8e663d1e4bc91fb34b1fa0612ed
anokata <anokata8@mail.ru>
2020-09-05 21:51:41 +0300

    upd

--- windows/autorunsoft.cmd
+++ windows/autorunsoft.cmd
@@ -4,3 +4,4 @@ d:\progs\vdesk\VDesk.exe on:2 noswitch:true run:"D:\progs\TelegramDesktop\Telegr
 REM Автозапуск vim with doc on 3
 vdesk create
 d:\progs\vdesk\VDesk.exe on:3 noswitch:true run:"D:\progs\Vim\vim82\gvim.exe"
+d:\progs\vdesk\VDesk.exe on:3 noswitch:true run:"D:\progs\Far\Far.exe"
