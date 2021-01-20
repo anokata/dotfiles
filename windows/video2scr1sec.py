b9fdd314253f4f20e180bb9efe1d8719c326db5e
anokata <anokata8@mail.ru>
2021-01-20 14:21:37 +0300

    upd

--- windows/video2scr1sec.py
+++ windows/video2scr1sec.py
@@ -14,9 +14,9 @@ def execute(cmd):
 
 
 ffmpeg="D:\\progs\\ffmpeg-2020-09-27-git-a3a6b56200-full_build\\bin\\ffmpeg.exe"
-opt=" -i {} -vf fps=1 {}\\{}%04d.jpg"
+opt=" -i \"{}\" -vf fps=1 -qscale:v 2 {}\\{}%04d.jpg"
 filename = sys.argv[1]
-shortname = "scr_" + sys.argv[1].replace(" ", "")
+shortname = "scr_" + sys.argv[1].replace(" ", "").replace("&", "")
 
 if not os.path.exists(shortname):
     os.makedirs(shortname)
@@ -26,3 +26,12 @@ print(cmdline)
 #start(cmdline)
 for path in execute(cmdline):
     print(path, end="")
+
+print("OK extract screenshots")
+
+zipper="D:\\progs\\7-Zip\\7z.exe" 
+zipopt=" a -sdel {}.zip {}"
+cmdline=zipper + zipopt.format(shortname, shortname)
+print(cmdline)
+start(cmdline)
+print("OK  zipped")
