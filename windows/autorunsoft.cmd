589bc48bb01ef1b75a9aff963e972081bd3697fd
anokata <anokata8@mail.ru>
2021-11-02 11:36:52 +0300

    upd

--- windows/autorunsoft.cmd
+++ windows/autorunsoft.cmd
@@ -1,6 +1,7 @@
 REM Autostart
-d:\progs\vdesk\VDesk.exe noswitch:true run:"D:\progs\Far\Far.exe"
-d:\progs\vdesk\VDesk.exe noswitch:true run:"D:\progs\Vim\vim82\gvim.exe"
+REM d:\progs\vdesk\VDesk.exe run:"D:\progs\Far\Far.exe"
+D:\progs\Far\Far.exe
+REM d:\progs\vdesk\VDesk.exe noswitch:true run:"D:\progs\Vim\vim82\gvim.exe"
 vdesk create
 d:\progs\vdesk\VDesk.exe on:2 noswitch:true run:"D:\progs\TelegramDesktop\Telegram.exe"
 REM vdesk create
