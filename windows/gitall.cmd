244549e21e3579231be558ba572066d932db811c
anokata <anokata8@mail.ru>
2021-11-07 09:48:35 +0300

    upd

--- windows/gitall.cmd
+++ windows/gitall.cmd
@@ -1,6 +1,6 @@
 cd D:\doc
-bash D:\dotfiles\windows\gitall 'upd'
+git-bash D:\dotfiles\windows\gitall 'upd'
 
 cd D:\dotfiles
-bash D:\dotfiles\windows\gitall 'upd'
+git-bash D:\dotfiles\windows\gitall 'upd'
 REM pause
