b94116c35083cf49266e91b1cda8c5b94e11c7cc
anokata <s.tikhomirov@ndmsystems.com>
2022-04-19 23:20:22 +0300

    WM: bspwm windows, polybar cfg, keys

--- linux/configs/polybar/launch.sh
+++ linux/configs/polybar/launch.sh
@@ -7,8 +7,8 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bottom 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bottom-2 2>&1 | tee -a /tmp/polybar2.log & disown
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar bottom -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bottom-2 -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
