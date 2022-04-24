0a45fd3ce6b2cd4eea4d92a0567c09cd32a5482a
anokata <s.tikhomirov@ndmsystems.com>
2022-04-24 12:43:59 +0300

    install script

--- linux/install.apps.sh
+++ linux/install.apps.sh
@@ -20,89 +20,26 @@ source $DOTFILES_SESSION/alias.sh
 #nvim -c :PlugInstall
 #sudo npm install -g prettier-eslint-cli
 
-apps_cli=('fzf' 'fd' 'bat' \
-'gsmartcontrol' \
-'htop' \
-'jq' \
-'libblkid1' \
-'mmv' \
-'ncat' \
-'ncdu' \
-'nvme-cli' \
-'ripgrep' \
-'calcurs' \
-'pwgen' \
-'gsmartcontrol' \
-'neovim' \
-)
 
-apps_wm=('xdotool' \
-'xmacro' \
-'xsel' \
-'hsetroot' \
-'gpick  ' \
-'bspwm' \
-'sxhkd' \
-'polybar' \
-'nm-applet' \
-'nmcli' \
-'rofi' \
-'lightdm-gtk-greeter' \
-'lightdm' \
-)
-
-apps_media=('keepassxc' \
-'transmission' \
-'imagemagick' \
-'peek' \
-'mpv' \
-'flameshot' \
-'scrot' \
-'feh' \
-'ffmpeg' \
-'zathura' \
-'zathura-djvu' \
-'mpd mpc  ' \
-)
-
-apps_net=('nm-connection-editor' \
-'tig' \
-'wavemon' \
-'aircrack-ng' \
-'nm-tool' \
-)
-
-apps_dev=('nodejs' \
-'npm' \
-)
-
-apps_network=('sshpass' \
-'wireshark' \
-'nmap' \
-'whois' \
-'arp-scan' \
-'netdiscover' \
-)
-
-apps_sys=('vmstat' \
-'nmon' \
-'atop' \
-'iotop' \
-'iostat' \
-'iftop' \
-)
 
 # npm install -g neovim
 # nvim -c :CocInstall coc-tsserver coc-json coc-html coc-css
 
 echo -e "Installing apps: cli"
-for app in ${apps_cli[@]}; do
-    echo $app
+#for app in ${apps_cli[@]}; do
+    #echo $app
     #sudo apt install $app
-done
-echo "done."
+#done
+#echo "done."
 
-sudo apt install fzf fd-find bat gsmartcontrol htop jq libblkid1 mmv ncat ncdu nvme-cli ripgrep calcurse pwgen gsmartcontrol neovim
+sudo apt install fzf fd-find bat gsmartcontrol htop jq libblkid1 mmv ncat ncdu nvme-cli ripgrep calcurse pwgen gsmartcontrol neovim -y
+sudo apt install xdotool xmacro xsel hsetroot gpick  bspwm sxhkd polybar nm-applet nmcli rofi lightdm-gtk-greeter lightdm -y
+sudo apt install keepassxc transmission imagemagick peek mpv flameshot scrot feh ffmpeg zathura zathura-djvu mpd mpc   -y
+sudo apt install nm-connection-editor tig wavemon aircrack-ng nm-tool -y
+sudo apt install vmstat nmon atop iotop iostat iftop -y
+sudo apt install sshpass wireshark nmap whois arp-scan netdiscover -y
+sudo apt install nodejs npm -y
+
 
 # need to load
 #obs
