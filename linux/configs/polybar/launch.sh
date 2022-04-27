340440343810653ce7dd1ecfeac517107819dfbf
anokata <s.tikhomirov@ndmsystems.com>
2022-04-27 23:25:02 +0300

    (wm) monitor detect and config bspwm and polybar with this data

--- linux/configs/polybar/launch.sh
+++ linux/configs/polybar/launch.sh
@@ -8,9 +8,12 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log ===>
-# polybar bottom -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
-# TODO $MONINTOR1
-polybar bottom-2 -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown
+source ~/dotfiles/linux/.env
+if [ -z $MONITOR_1 ]; then
+    polybar bottom -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
+else 
+    polybar bottom-2 -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown
+fi
 # polybar bottom-tray -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
