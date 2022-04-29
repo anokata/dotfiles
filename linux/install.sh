65858746a70c88e049baa23db80f26fd201daab9
anokata <s.tikhomirov@ndmsystems.com>
2022-04-29 11:54:51 +0300

    env git email

--- linux/install.sh
+++ linux/install.sh
@@ -4,6 +4,16 @@ source ~/dotfiles/linux/.bashrc
 sudo apt install git
 echo 'source ~/dotfiles/linux/.bashrc' >> ~/.bashrc
 echo 'source ~/dotfiles/linux/.env' >> ~/.profile
+
+git config --global user.name anokata
+git config --global user.email $WORK_EMAIL
+
+cd ~/dotfiles
+git config user.email $KSI_EMAIL
+cd -
+
+# TODO: ssh keys
+
 ./install.apps.sh
 ./install.dirs.sh
 source ~/dotfiles/linux/.env
---
+++ linux/install.work.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source $DOTFILES_LINUX/.env
+mkdir $WORK_DIR
+cd $WORK_DIR
+NDM_URL=git@github.com:ndmsystems/ndw3-without-isw.git
+# git clone $NDM_URL
+# cd
+# npm install
