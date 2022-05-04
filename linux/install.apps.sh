f94cd4c0883bb3f2ff8aab7f91508b7f0560f3c6
anokata <s.tikhomirov@ndmsystems.com>
2022-05-04 23:04:56 +0300

    7z

--- linux/install.apps.sh
+++ linux/install.apps.sh
@@ -45,16 +45,16 @@ sudo pip3 install pynvim
 i pavucontrol
 i fonts-firacode 
 i exuberant-ctags
-i 7z pv
+i p7zip-full pv
 
 # VSCode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code # or code-insiders
+sudo apt install apt-transport-https -y
+sudo apt update -y
+sudo apt install code -y
 
 #i cmake
 
