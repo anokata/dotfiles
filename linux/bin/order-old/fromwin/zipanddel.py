1bb59e83a02507f7b2647d93bd635b6661ad916c
anokata <s.tikhomirov@ndmsystems.com>
2022-05-04 17:37:54 +0300

    rename

--- linux/bin/order-old/fromwin/zipanddel.py
+++ linux/bin/order-old/fromwin/zipanddel.py
@@ -5,7 +5,7 @@ import os
 shortname = sys.argv[1].replace(" ", "").replace("&", "")
 filename = sys.argv[1]
 
-zipper="D:\\progs\\7-Zip\\7z.exe" 
+zipper="7z" 
 zipopt=" a -sdel -mx1 {}.zip \"{}\""
 
 cmdline=zipper + zipopt.format(shortname, filename)
