098a9260663258b07d52a9c073d3eb2d372eded6
anokata <s.tikhomirov@ndmsystems.com>
2022-06-05 08:47:36 +0300

    polybar

--- linux/configs/polybar/launch.sh
+++ linux/configs/polybar/launch.sh
@@ -8,6 +8,7 @@ source ~/dotfiles/linux/.env
 if [ -z $MONITOR_1 ]; then
     polybar bottom -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
 else 
+    polybar bottom -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
     polybar bottom-2 -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown
 fi
 # polybar bottom-tray -c ~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown
