02532ab26453bd460bffad2f71d925e210ec6779
anokata <s.tikhomirov@ndmsystems.com>
2022-06-12 15:13:30 +0300

    fix tablet area for new monitor

--- linux/bin/session/tmux.sh
+++ linux/bin/session/tmux.sh
@@ -13,7 +13,7 @@ function _tmux_run () {
             tmux new -s $TMUX_SESSION_NAME -d
             tmux new-window -c $DOTFILES_LINUX
             tmux new-window -c $WORK_DIR_CURRENT
-            tmux split-window
+            tmux split-window -c $WORK_DIR_CURRENT2
             # tmux split-window -c ~/sdata/
             # tmux new-window vifm
             # tmux new-window cmus
