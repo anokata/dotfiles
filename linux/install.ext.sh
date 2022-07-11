0b392839b228b1b8e3582093a0fa66a58834df9b
anokata <s.tikhomirov@ndmsystems.com>
2022-07-11 11:50:34 +0300

    cfg

--- linux/install.ext.sh
+++ linux/install.ext.sh
@@ -63,6 +63,9 @@ i gnugo
 i gparted
 i ncmpcpp
 i rsync
+
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp
 # atpi snapd
 # sudo snap install slack
 # sudo snap set system refresh.retain=2
