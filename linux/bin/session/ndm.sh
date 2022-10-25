c9522c8d052f89a67f8971af1ac552c13c9871f9
anokata <quartenium@gmail.com>
2022-10-25 10:57:11 +0300

    chg

--- linux/bin/session/ndm.sh
+++ linux/bin/session/ndm.sh
@@ -5,10 +5,10 @@ alias cdns="cd $WORK_DIR/ndw3-storybook/"
 
 alias ndm-keenetic-telnet='telnet 192.168.1.1'
 alias keenetic-telnet='telnet 192.168.1.1'
-alias keenetic-ssh='ssh admin@192.168.1.1 -p 2022'
+alias keenetic-ssh='ssh admin@192.168.2.1 -p 2022'
 alias ndm-cbox='ssh sernam@cbox.ndm9.net'
 alias ndm-ssh-local='ssh admin@192.168.1.1 -p 2022'
-alias ndm-ssh-local-default='sshpass -f $PASSWORD_DEFAULT ssh admin@192.168.1.1 -p 2022'
+#alias ndm-ssh-local-default='sshpass -f $PASSWORD_DEFAULT ssh admin@192.168.1.1 -p 2022'
 alias ndm-keenetic-ssh-local='ssh admin@192.168.1.1 -p 2022'
 alias ndm-keenetic-show-arp='ndm-ssh-local show ip arp'
 alias ndm-keenetic-show-route='ndm-ssh-local show ip route'
