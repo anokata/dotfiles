eb83f5fd6ed99fb0fdb18ee6acc2b3b43f1e0de5
anokata <quartenium@gmail.com>
2022-12-05 10:11:03 +0300

    replace keenetic ip with var

--- linux/bin/session/ndm.sh
+++ linux/bin/session/ndm.sh
@@ -1,19 +1,18 @@
-
 alias cdn3="cd $WORK_DIR/ndw3-without-isw/"
 alias cdn2="cd $WORK_DIR/ndw2/"
 alias cdns="cd $WORK_DIR/ndw3-storybook/"
 
-alias ndm-keenetic-telnet='telnet 192.168.1.1'
-alias keenetic-telnet='telnet 192.168.1.1'
-alias keenetic-ssh='ssh admin@192.168.2.1 -p 2022'
-alias ndm-cbox='ssh sernam@cbox.ndm9.net'
-alias ndm-ssh-local='ssh admin@192.168.1.1 -p 2022'
-#alias ndm-ssh-local-default='sshpass -f $PASSWORD_DEFAULT ssh admin@192.168.1.1 -p 2022'
-alias ndm-keenetic-ssh-local='ssh admin@192.168.1.1 -p 2022'
+alias ndm-keenetic-telnet="telnet $KEENETIC_IP"
+alias keenetic-telnet="telnet $KEENETIC_IP"
+alias keenetic-ssh="ssh admin@$KEENETIC_IP -p 2022"
+alias ndm-cbox="ssh sernam@cbox.ndm9.net"
+alias ndm-ssh-local="ssh admin@$KEENETIC_IP -p 2022"
+#alias ndm-ssh-local-default='sshpass -f $PASSWORD_DEFAULT ssh admin@$KEENETIC_IP -p 2022'
+alias ndm-keenetic-ssh-local="ssh admin@$KEENETIC_IP -p 2022"
 alias ndm-keenetic-show-arp='ndm-ssh-local show ip arp'
 alias ndm-keenetic-show-route='ndm-ssh-local show ip route'
 
-alias ndm-start-local="npm start 192.168.1.1"
+alias ndm-start-local="npm start $KEENETIC_IP"
 alias ndm-start-mobile="npm start 192.168.22.14"
 alias ndm-test="npm run ng-test"
 alias ndm-add-vland="ndm-ssh-local-default interface GigabitEthernet0/Vlan5"
@@ -31,9 +30,9 @@ alias ndm-task-status-done="echo '(done, ndw3-without-isw)' | xclip -sel c"
 alias ndm-task-status-review="echo '(review, ndw3-without-isw)' | xclip -sel c"
 alias ndm-task-status-dev="echo '(dev, ndw3-without-isw)' | xclip -sel c"
 alias ndm-test-domain="npm run ng-test src/domain/pages/"
-alias ndm-opkg="ssh root@192.168.1.1 -p 222"
+alias ndm-opkg="ssh root@$KEENETIC_IP -p 222"
 
 alias ndm-git-clone-ndw2="cd $WORK_DIR; git clone $WORK_REPO_NDW2"
 alias ndm-git-clone-ndw3="cd $WORK_DIR; git clone $WORK_REPO_NDW3"
 
-alias ndm-start-local2="npx gulp serve -P 192.168.2.1"
+alias ndm-start-local2="npx gulp serve -P $KEENETIC_IP"
