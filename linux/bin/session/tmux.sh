32627aedf43438f82af4f1a477df8b9938ebb8b6
anokata <quartenium@gmail.com>
2024-10-29 09:37:45 +0800

    tmux-cfg

--- linux/bin/session/tmux.sh
+++ linux/bin/session/tmux.sh
@@ -19,12 +19,12 @@ function _tmux_run () {
             tmux split-window -c $WORK_DIR_CURRENT5
             tmux new-window -c $WORK_DIR_CURRENT -n ndw4
             tmux split-window -c $WORK_DIR_CURRENT
-            tmux select-window -t 1
-            tmux attach -t $TMUX_SESSION_NAME -c $DOTFILES_LINUX
             tmux new-window -c ~/gits -n gits
             tmux new-window -c ~/pwn -n pwn
             tmux split-window -c ~/pwn
             tmux new-window -c ~/Downloads -n dwl
+            tmux select-window -t 1
+            tmux attach -t $TMUX_SESSION_NAME -c $DOTFILES_LINUX
         fi
     #else
         #tmux a
