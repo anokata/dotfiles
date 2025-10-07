ee8cf16a082843a4692eb934b305135f4405e6c5
anokata <quartenium@gmail.com>
2025-10-07 12:16:11 +0800

    (clean) bash bindings

--- linux/bin/session/bind.fish
+++ linux/bin/session/bind.fish
@@ -1,8 +1,18 @@
 # fish_key_reader
 # to get key code(in bash): press ctrl-v and key; replace ^[ to \e
+# and use fish_key_reader
 
-bind ctrl-n lf
-bind --mode insert ctrl-n lf
+# alt-h (or f1) shows the manual page for the current command, if one exists.
+# alt-l lists the contents of the current directory,
+# alt-w prints a short description of the command under the cursor
+# alt-s Prepends sudo to the current commandline
+# ctrl-n accepts the autosuggestion.
+
+# File Managment: LF
+# bind ctrl-n lf
+# bind --mode insert ctrl-n lf
+bind \e\[15~ lf # F5
+bind -M insert \e\[15~ lf # F5
 
 # F1 - open session: dotfiles
 bind -k f1 'cd $DOTFILES; $EDITOR'
@@ -11,9 +21,37 @@ bind --mode insert -k f1 'cd $DOTFILES; $EDITOR'
 bind \eOQ 'cd $NOTES_DIR; $EDITOR'
 bind --mode insert \eOQ 'cd $NOTES_DIR; $EDITOR' # F2 - open notes
 
-
 # TODO
-# bind -x '"\C-b":"bashrc-reload "' # 
-# bind  cd ..
-# bind  cd -
+# vim sessions
+# bind -x '"\eOP":"cd $DOTFILES; $EDITOR"' # F1 - open session: dotfiles
+# bind -x '"\eOQ":"cd $NOTES_DIR; $EDITOR"' # F2 - notes
+
+# configs
+# bind -x '"\e[1;5P":"$EDITOR $DOTFILES_CONFIGS/.vimrc"' # Ctrl-F1
+# bind -x '"\e[1;5Q":"$EDITOR $DOTFILES_LINUX/.bashrc"' # Ctrl-F2
+
+# fzf and vim
+# bind -x '"\C-f":"$EDITOR $(fzf)"' # C-e fzf and open in vim
+# bind -x '"\C-g":"fzf-ueberzogen.sh"' # image preview
+# bind -x '"\C-o":"cd $NOTES_DIR; $EDITOR $(fzf)"' # C-e fzf and open in vim
+
+# PS
+# bind '"\C-k":"pkill "' # 
 # bind '"\C-k":"pkill "' # prepand pkill ; alt-k
+# bind -x '"\C-y":"htop "' # 
+# bind -x '"\e[20~":"htop "' # <F9>
+# TODO btop s-tui 
+# TODO bind to 'git status'  and git status; git diff --shortstat master  AND git pull
+
+# IDE
+# bind -x '"\eg":"tig"' # tig by c-s-g
+# bind -x '"\e[19~":"tig"' # tig by <F8>
+
+# lfcd
+
+bind ctrl-b fish-config-reload
+bind --mode insert ctrl-b fish-config-reload
+
+bind -M insert \e\[1\;5A 'cd ..' execute # ctrl-up
+
+bind -M insert \e\[1\;5D 'cd -' execute # ctrl-left
--- linux/bin/session/bind.sh
+++ linux/bin/session/bind.sh
@@ -26,10 +26,11 @@ bind -x '"\C-b":"bashrc-reload "' #
 bind -x '"\eOP":"cd $DOTFILES; $EDITOR"' # F1 - open session: dotfiles
 bind -x '"\eOQ":"cd $NOTES_DIR; $EDITOR"' # F2 - notes
 bind -x '"\eOS":"cd $DOTFILES_CONFIGS; $EDITOR"' # F4 - configs
-bind -x '"\e[1;5S":"cd $VIM_DIR_CONFIGS; $EDITOR"' # C-F4 - vim
+# bind -x '"\e[24~":"cd $VIM_DIR_CONFIGS; $EDITOR"' # F12 - vim
 
 # configs
 bind -x '"\e[1;5P":"$EDITOR $DOTFILES_CONFIGS/.vimrc"' # C-F1
+# bind -x '"\e[1;3P":"$EDITOR $DOTFILES_CONFIGS/.vimrc"' # 
 bind -x '"\e[1;5Q":"$EDITOR $DOTFILES_LINUX/.bashrc"' # C-F2
 
 # fzf and vim
@@ -40,13 +41,12 @@ bind -x '"\C-o":"cd $NOTES_DIR; $EDITOR $(fzf)"' # C-e fzf and open in vim
 # PS
 bind '"\C-k":"pkill "' # 
 bind -x '"\C-y":"htop "' # 
-# TODO btop s-tui 
-bind -x '"\e[19~":"tig"' # tig by <F8>
 bind -x '"\e[20~":"htop "' # <F9>
+# TODO btop s-tui 
 # TODO bind to 'git status'  and git status; git diff --shortstat master  AND git pull
 
 # IDE
 bind -x '"\eg":"tig"' # tig by c-s-g
+bind -x '"\e[19~":"tig"' # tig by <F8>
 
-bind '"\C-a":"tmux a\C-m"' # ??? tmux
-bind -x '"\e[1;3P":"$EDITOR $DOTFILES_CONFIGS/.vimrc"' # 
+# bind '"\C-a":"tmux a\C-m"' # ??? tmux
